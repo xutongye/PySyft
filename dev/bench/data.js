1e8b0b0de26e6025ef4cd5250acd83fea5d952b0
github-action-benchmark <github@users.noreply.github.com>
2020-12-15 03:33:00 +0000

    add Pytest-benchmarks (pytest) benchmark result for fe71a27a745d205981e44177d092d326f7e7c1af

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,6 +1,6 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1603719793392,
-  "repoUrl": "https://github.com/OpenMined/PySyft",
+  "lastUpdate": 1608003180891,
+  "repoUrl": "https://github.com/xutongye/PySyft",
   "entries": {
     "Python Benchmark with pytestbenchmark": [
       {
@@ -177,6 +177,39 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.006991693104589301",
             "extra": "mean: 1.2066466737999917 sec\nrounds: 5"
+          }
+        ]
+      }
+    ],
+    "Pytest-benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "email": "anubhavraj.08@gmail.com",
+            "name": "Anubhav Raj Singh",
+            "username": "aanurraj"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "fe71a27a745d205981e44177d092d326f7e7c1af",
+          "message": "add github benchmark-actions support (#4905)\n\n* added benchmark_actions support\r\n\r\n* added requirements\r\n\r\n* fixes\r\n\r\n* fixes\r\n\r\n* yml fixes\r\n\r\n* Trying different github secrets\r\n\r\n* Trying BENCHMARK_TOKEN\r\n\r\n* Changing token path\r\n\r\n* Trying different token setting\r\n\r\n* Back to secrets.GITHUB_TOKEN\r\n\r\nCo-authored-by: Madhava Jay <me@madhavajay.com>",
+          "timestamp": "2020-12-13T15:19:24+10:00",
+          "tree_id": "4959ab2de2e0399c8254a9e7c2e571e60235e71c",
+          "url": "https://github.com/xutongye/PySyft/commit/fe71a27a745d205981e44177d092d326f7e7c1af"
+        },
+        "date": 1608003179988,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/benchmarks/pytest_benchmarks/bench_test.py::test_string_serde",
+            "value": 1223.7626857450953,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00016318988769177854",
+            "extra": "mean: 817.1518968901589 usec\nrounds: 611"
           }
         ]
       }
